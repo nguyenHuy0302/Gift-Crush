906dddb59594c2aa47e1093d572fc5c2286154fb
unknown <nguyendinhhuy0302@gmail.com>
2023-12-11 14:58:04 +0700

    up

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Em à, anh muốn nói với em vài điều thôi !',
+    introDesc: `Người ta nói cuộc đời này có hai thứ ta không nên bỏ lỡ
+    1 là chuyến xe cuối cùng về nhà 
+    2 là người thật lòng yêu thương mình`,
+    btnIntro: 'Bấm vào đây nè ... ',
+    title: 'Anh hy vọng em đừng bỏ lỡ anh 😙',
+    desc: 'Do you love me?  ',
+    btnYes: ' yess<33',
+    btnNo: 'Không, Anh mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà là em cũng thích anh mà',
+    messDesc: ' vậy thôi mình ib nói chuyện nha . acc anh là Nguyễn Đình Huy ',
+    btnAccept: 'dạ anh <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100029030894084' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
